c6e765b312656c9dd57ed028d4b41c61f2a14a23
UGLimusic <uglimusic@gmail.com>
2021-10-28 11:09:12 +0200

    Added comments

--- entities.py
+++ entities.py
@@ -1,5 +1,8 @@
 from random import randint
 
+
+# constantes propres aux entités
+
 MATURITY_AGE = 100
 MAX_ALIENS = 50
 GRID_WIDTH = 80
@@ -7,19 +10,19 @@ GRID_HEIGHT = 80
 
 
 class Predator:
-    population = []
+    population = [] # variable de classe pour stocker tous les prédateurs
 
     @classmethod
-    def update_all(cls):
+    def update_all(cls): # MAJ de tous les prédateurs
         for predator in cls.population:
             predator.update()
 
-    def __init__(self, x, y):
+    def __init__(self, x, y): # tout bête
         self.x = x
         self.y = y
         Predator.population.append(self)
 
-    def update(self):
+    def update(self): # voilà ce que fait un prédateur
         self.move()
         self.predate()
 
@@ -29,19 +32,19 @@ class Predator:
         self.x = min(max(0, self.x), GRID_WIDTH - 1)
         self.y = min(max(0, self.y), GRID_HEIGHT - 1)
 
-    def predate(self):
+    def predate(self): # bien regarder comment on gère la disparition des Aliens
         casualties = []
         for i in range(len(Alien.population)):
             if abs(self.x - Alien.population[i].x) + abs(self.y - Alien.population[i].y) < 5:
                 casualties.append(i)
-        Alien.kill(casualties)
+        Alien.kill(casualties) # on a la liste des Aliens à tuer, on la passe à la méthode de classe Alien.kill
 
 
 class Alien:
     population = []
 
     @classmethod
-    def kill(cls, casualties):
+    def kill(cls, casualties): # pour gérer les morts
         new_population = []
         for i in range(len(Alien.population)):
             if i not in casualties:
@@ -49,22 +52,25 @@ class Alien:
         Alien.population = new_population
 
     @classmethod
-    def update_all(cls):
+    def update_all(cls): # pareil que pour les prédateurs en plus compliqué
 
         new_individuals = []
         for i in range(len(cls.population)):
-            cls.population[i].update()
+            cls.population[i].update() # chaque Alien est MAJ
+
+            # Ensuite on regarde ceux qui sont matures et proches pour les faire se reproduire
             if len(cls.population) < MAX_ALIENS:
                 if cls.population[i].maturity == MATURITY_AGE:
                     for j in range(i + 1, len(cls.population)):
                         if cls.population[j].maturity == MATURITY_AGE:
                             if abs(cls.population[i].x - cls.population[j].x) + abs(
                                     cls.population[i].y - cls.population[j].y) < 4:
-                                new_individuals.append((i, j))
+                                new_individuals.append((i, j)) # on utilise une nouvelle liste pour ne pas
+                                # << bousiller >> celle sur laquelle on boucle
 
             for (i, j) in new_individuals:
                 if len(cls.population) < MAX_ALIENS:
-                    cls.population[i] + cls.population[j]
+                    cls.population[i] + cls.population[j] # et c'est là qu'on la change, à la fin.
 
     def __init__(self, x: int, y: int):
         self.x = x
@@ -86,7 +92,7 @@ class Alien:
         self.maturity += 1
         self.maturity = min(self.maturity, MATURITY_AGE)
 
-    def __add__(self, other):
+    def __add__(self, other): # surcharge de << + >> qui permet de faire a1 + a2 --> un petit bébé alien trop mignon !
         self.maturity = 0
         other.maturity = 0
         return Alien((self.x + other.x) // 2, (self.y + other.y) // 2)
--- test_entities.py
+++ test_entities.py
@@ -1,34 +1,38 @@
 import pygame
 from entities import *
 
+# Ici c'est du pygame : comme du p5, en plus rapide
+
+
 GRID_SIZE = 5
 SCREEN_WIDTH, SCREEN_HEIGHT = GRID_WIDTH * GRID_SIZE, GRID_HEIGHT * GRID_SIZE
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
-
-pygame.init()
-surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-clock = pygame.time.Clock()
+BLACK = (0, 0, 0)
+pygame.init()  # on init pygame
+surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # fenetre d'écran
+clock = pygame.time.Clock()  # horloge
 FPS = 60
 
 
-def draw_predators():
+def draw_predators() -> None:
     for predator in Predator.population:
         pygame.draw.rect(surface, RED, (predator.x * GRID_SIZE, predator.y * GRID_SIZE, GRID_SIZE, GRID_SIZE))
+        # pygame.draw.rect(surface sur laquelle on dessine, couleur, (top,left,width,height))
 
 
-def draw_aliens():
+def draw_aliens() -> None:
     for alien in Alien.population:
         pygame.draw.rect(surface, BLUE, (alien.x * GRID_SIZE, alien.y * GRID_SIZE, GRID_SIZE, GRID_SIZE))
 
 
-def draw():
-    surface.fill((0, 0, 0, 10))
-    Alien.update_all()
-    Predator.update_all()
+def draw() -> None:
+    surface.fill(BLACK)  # on efface tout
     draw_aliens()
-    draw_predators()
+    draw_predators()  # et on recommence
 
+
+# ici on crée les bebêtes
 
 Alien(0, GRID_HEIGHT - 1)
 Alien(GRID_WIDTH - 1, 0)
@@ -37,13 +41,17 @@ Alien(GRID_WIDTH // 2 - 3, GRID_HEIGHT // 2 + 3)
 Predator(0, 0)
 Predator(GRID_WIDTH - 1, GRID_HEIGHT - 1)
 
-go_on = True
 
+
+# voilà une boucle pygame typique :
+
+go_on = True
 while go_on:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            go_on = False
-    draw()
-    pygame.display.update()
-    clock.tick(60)
-pygame.quit()
+    for event in pygame.event.get(): # on traite la file d'évènements
+        if event.type == pygame.QUIT: # si on a un évènement de type quitter
+            go_on = False # on casse la boucle while
+    draw() # on dessine
+    pygame.display.update() #IMPORTANT : on update l'écran
+    clock.tick(FPS) # l'horloge fait bip FPS fois par seconde donc on attend qu'elle bippe
+
+pygame.quit() # Un coup de balai en partant ça ne fait pas de mal.
